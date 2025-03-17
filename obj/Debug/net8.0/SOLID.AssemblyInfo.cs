876d4ca1c434cec22d2218ed4d9b661c97a99cb0
IghorRhafael <ighorrhafael@hotmail.com>
2025-03-17 16:13:15 +0000

    Update AssemblyInfo to include detailed informational version and regenerate cache

--- obj/Debug/net8.0/SOLID.AssemblyInfo.cs
+++ obj/Debug/net8.0/SOLID.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b63a79a515d94249d99bdcd96baf18fcbe942c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOLID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
